d0706b499b0d3e7da7b21ad8b3758be6771e2708
Alexhawak <hloji999@gmail.com>
2025-01-13 13:07:38 +0530

    almost finish

---
+++ app.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.feedback');
+
+const cards = document.querySelectorAll('.cards');
+
+const submit = document.querySelector('#submit');
+
+let rating = '';
+
+cards.forEach((ele) => {
+  ele.addEventListener('click', (element) => {
+    removeActive();
+
+    ele.classList.add('active');
+
+    rating = element.target.id;
+  });
+});
+
+submit.addEventListener('click', () => {
+  if (rating !== '') {
+    container.innerHTML = `
+        <strong>Thank you!</strong>
+        <br>
+        <strong>Feedback: ${rating}</strong>
+        <p>We'll use your feedback to improve our customer support.</p>
+        `;
+    rating = '';
+  } else {
+    alert('Please select rating');
+  }
+});
+function removeActive() {
+  cards.forEach((ele) => {
+    ele.classList.remove('active');
+  });
+}
